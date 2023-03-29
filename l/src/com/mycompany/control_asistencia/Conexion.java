52af65234d36fee05a8ae86f9ee434d75d922bd2
santiago566 <125104666+santiago566@users.noreply.github.com>
2023-03-29 09:20:36 -0500

    inicio

--- l/src/com/mycompany/control_asistencia/Conexion.java
+++ l/src/com/mycompany/control_asistencia/Conexion.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.control_asistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Santiago
  */
 public class Conexion {
     
+    Connection conectar =null;
+    String usuario ="root";
+    String contrasenia="root";
+    String bd= "registro_asistencia";
+    String ip="localhost";
+    String puerto="3306";
+    
+    String cadena ="jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    public Connection estableceConexion(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        conectar=DriverManager.getConnection(cadena,usuario,contrasenia);
+        //JOptionPane.showMessageDialog(null," se conecto a la base de datos ");
+
+    } catch(Exception e){
+        JOptionPane.showMessageDialog(null,"No se conecto a la base de datos , error: "+ e.toString());
+    }
+    return conectar;
+    }
+    
+
 }
--- l/src/com/mycompany/control_asistencia/Control_asistencia.java
+++ l/src/com/mycompany/control_asistencia/Control_asistencia.java
@@ -4,10 +4,30 @@
  */
 package com.mycompany.control_asistencia;
 
+
+import diseños.Docente;
+import diseños.vista_admin;
+import logueo.inicio;
+
 /**
  *
  * @author Santiago
  */
 public class Control_asistencia {
-    
+
+    public static void main(String[] args) {
+        
+        /*Registro_docentes docentes= new Registro_docentes();
+        docentes.setVisible(true);*/
+        /*vista_admin ad= new vista_admin();
+        ad.setVisible(true);*/
+        
+        /*Docente doc= new Docente();
+        doc.setVisible(true);*/
+       
+        
+        
+       /*Conexion objetoConexion = new Conexion();
+        objetoConexion.estableceConexion();*/
+    }//comentario para saber si se subio
 }
